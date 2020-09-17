f521c3b431355ea8613779fbea4b88606bbfb31e
Geert-Jan Knapen <geertjanknapen@gmail.com>
2020-09-17 20:11:20 +0200

    Almost fully random MOTD's from config file.

--- src/me/geertjanknapen/PlaytimeBan/listeners/PingListener.java
+++ src/me/geertjanknapen/PlaytimeBan/listeners/PingListener.java
@@ -19,18 +19,12 @@ public class PingListener implements Listener{
 		
 		Bukkit.getPluginManager().registerEvents(this, plugin);
 	}
-	
+
 	@EventHandler
 	public void onPing(ServerListPingEvent e) {
 		// Create an array with some motd's and a random to select a random motd
-		String[] motdArr = 
-			{
-					"&aSpigot 1.16.2 &6Plugin Test Server",
-					"&5Geert-Jan's Plugin Test Paradijs!",
-					"&c&bPlugin's?! &d&bWe got 'em!"
-			};
 		Random rand = new Random();
-		int randInt = rand.nextInt(motdArr.length);
-		e.setMotd(Utils.chat(motdArr[randInt]));
+		int randInt = rand.nextInt(4);
+		e.setMotd(Utils.chat(plugin.getConfig().getString("motd_" + randInt)));
 	}
 }
